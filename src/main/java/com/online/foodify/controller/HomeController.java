70d67c83ebd4b57203ce1af1acd7560db71511d5
johari02shubhi <84064254+johari02shubhi@users.noreply.github.com>
2021-05-24 18:53:27 +0530

    Add files via upload

---
+++ src/main/java/com/online/foodify/controller/HomeController.java
@@ -0,0 +1,39 @@
+package com.online.foodify.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.online.foodify.service.CategoryService;
+import com.online.foodify.service.FoodProductService;
+
+@Controller
+public class HomeController {
+	@Autowired
+	CategoryService categoryService;
+	
+	@Autowired
+	FoodProductService foodProductService;
+	
+	@GetMapping({"/", "/home"})
+	public String home(Model model) {
+		return "index";
+	}
+	
+	@GetMapping("/shop")
+	public String shop(Model model) {
+		model.addAttribute("categories", categoryService.getAllCategory());
+		model.addAttribute("foodProducts", foodProductService.getAllFoodProduct());
+		return "shop";
+	}
+	
+	@GetMapping("/shop/category/{id}")
+	public String shopByCategory(Model model, @PathVariable int id) {
+		model.addAttribute("categories", categoryService.getAllCategory());
+		model.addAttribute("foodProducts", foodProductService.getAllFoodByCategoryId(id));
+		return "shop";
+	}
+
+}
